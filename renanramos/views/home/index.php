cb5c6c16fe7fb81ce1af38caf1ae42bd9ca6e6d0
Renan Ramos <renan_sluder@hotmail.com>
2014-08-25 01:01:25 -0300

    Att

---
+++ renanramos/views/home/index.php
@@ -0,0 +1,21 @@
+<?php
+    // echo "";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>.: Renan Ramos :.</title>
+</head>
+<body style="background-image: url('<?php echo base_url('public/img/fundo.png'); ?>'); background-repeat: repeat">
+<div id="container">
+	<div>
+		<center><br /><br />
+			<h1>Seja bem vindo!</h1><br /><br /><br />
+			<p><strong>Ops!...</strong></p>
+			<p><strong>Infelizmente o site está em construção...</strong></p>
+			<p>Tente novamente mais tarde<br /><strong>;D</strong></p>
+		</center>
+	</div>
+</body>
+</html>
